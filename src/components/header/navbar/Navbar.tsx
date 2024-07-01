96a39563ce3ca0e189ba53793fb71efd0595b1f2
brenocrepaldi <brenogaia2004@gmail.com>
2024-07-01 19:45:54 -0300

    Refactor the navbar to handle the scroll
    - The scroll goes to the center of the section selected

--- src/components/header/navbar/Navbar.tsx
+++ src/components/header/navbar/Navbar.tsx
@@ -3,8 +3,23 @@ import { NavbarProps } from "../../../types";
 import "./Navbar.css";
 
 export function Navbar({ navbar }: { navbar: NavbarProps[] }) {
+	const handleScroll = (
+		event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+	) => {
+		event.preventDefault();
+		const targetId = event.currentTarget.getAttribute("href")!.substring(1);
+		const targetElement = document.getElementById(targetId);
+
+		if (targetElement) {
+			targetElement.scrollIntoView({
+				behavior: "smooth",
+				block: "center",
+			});
+		}
+	};
+
 	const navItems = navbar.map(({ href, title }, index) => (
-		<a key={index} href={`#${href}`}>
+		<a key={index} href={`#${href}`} onClick={handleScroll}>
 			<p className="navbar-item">{title}</p>
 		</a>
 	));
