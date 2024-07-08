c278d30b37098fbce4e64abbf5571dae9b323002
brenocrepaldi <brenogaia2004@gmail.com>
2024-07-07 21:38:10 -0300

    Refactor experience files

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,5 +1,6 @@
 import { ExperienceProps, JobProps } from "../../types";
 import "./Experience.css";
+import { JobItem } from "./job-list/job-item/JobItem";
 
 export function Experience({ title, jobs }: ExperienceProps) {
 	return (
@@ -7,40 +8,7 @@ export function Experience({ title, jobs }: ExperienceProps) {
 			<h2>{title}</h2>
 			<ul className="job-list" role="list">
 				{jobs.map((job: JobProps, index: number) => (
-					<a
-						href="https://www.linkedin.com/company/espectro-ltda-/"
-						target="_blank"
-					>
-						<li key={index} className="job-item" role="listitem">
-							<p className="job-duration">{job.duration}</p>
-							<div className="job-details">
-								<h3 className="job-title">
-									{job.position} <strong>·</strong> {job.company}
-									<svg
-										className="job-arrow"
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 20 20"
-										fill="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											fill-rule="evenodd"
-											d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-											clip-rule="evenodd"
-										></path>
-									</svg>
-								</h3>
-								<p className="job-description">{job.description}</p>
-								<ul className="technology-list">
-									{job.technologies.map((tech: string, index: number) => (
-										<li key={index} className="technology-item">
-											<p className="technology-text">{tech}</p>
-										</li>
-									))}
-								</ul>
-							</div>
-						</li>
-					</a>
+					<JobItem key={index} job={job} />
 				))}
 			</ul>
 		</section>
---
+++ src/components/experience/job-list/job-item/JobItem.tsx
@@ -0,0 +1,42 @@
+import { JobProps } from "../../../../types";
+
+interface JobItemProps {
+	key: number;
+	job: JobProps;
+}
+
+export function JobItem({ key, job }: JobItemProps) {
+	return (
+		<a href="https://www.linkedin.com/company/espectro-ltda-/" target="_blank">
+			<li key={key} className="job-item" role="listitem">
+				<p className="job-duration">{job.duration}</p>
+				<div className="job-details">
+					<h3 className="job-title">
+						{job.position} <strong>·</strong> {job.company}
+						<svg
+							className="job-arrow"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+								clip-rule="evenodd"
+							></path>
+						</svg>
+					</h3>
+					<p className="job-description">{job.description}</p>
+					<ul className="technology-list">
+						{job.technologies.map((tech: string, index: number) => (
+							<li key={index} className="technology-item">
+								<p className="technology-text">{tech}</p>
+							</li>
+						))}
+					</ul>
+				</div>
+			</li>
+		</a>
+	);
+}
