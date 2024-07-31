6f407f254ab7e541f5fc87ebaff508fc7ddc36d9
brenocrepaldi <brenogaia2004@gmail.com>
2024-07-30 22:44:30 -0300

    Refactors contact section - content and style

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,6 +1,25 @@
 import { ContactsProps } from '../../types';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+	faLinkedin,
+	faGithub,
+	IconDefinition,
+} from '@fortawesome/free-brands-svg-icons';
+
 import './contact.css';
+
+interface IconMap {
+	[key: string]: IconDefinition;
+}
+
+const icons: IconMap = {
+	email: faEnvelope,
+	phone: faPhone,
+	linkedin: faLinkedin,
+	github: faGithub,
+};
 
 export function Contact({
 	title,
@@ -14,35 +33,61 @@ export function Contact({
 			<h2>{title}</h2>
 			<div className="contact-list">
 				<div className="contact-item">
-					<strong>Email:</strong>
-					<a href={`mailto:${email.content}`} target="_blank">
-						{email.content}
+					<a
+						href={`mailto:${email.content}`}
+						target="_blank"
+						className="contact-icon-container"
+					>
+						<FontAwesomeIcon
+							icon={icons['email']}
+							className="contact-item-icon"
+						/>
 					</a>
+					<strong>{email.title}</strong>
 				</div>
 
 				<div className="contact-item">
-					<strong>Telefone</strong>
 					<a
 						href={`https://web.whatsapp.com/send?phone=${phone.content}`}
 						target="_blank"
+						className="contact-icon-container"
 					>
-						{phone.content}
+						<FontAwesomeIcon
+							icon={icons['phone']}
+							className="contact-item-icon"
+						/>
 					</a>
+					<strong>{phone.title}</strong>
 				</div>
 
 				<div className="contact-item">
-					<strong>LinkedIn:</strong>
-
-					<a href={linkedin.content} target="_blank" rel="noopener noreferrer">
-						{linkedin.content}
+					<a
+						href={linkedin.content}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="contact-icon-container"
+					>
+						<FontAwesomeIcon
+							icon={icons['linkedin']}
+							className="contact-item-icon"
+						/>
 					</a>
+					<strong>{linkedin.title}</strong>
 				</div>
 
 				<div className="contact-item">
-					<strong>GitHub:</strong>
-					<a href={github.content} target="_blank" rel="noopener noreferrer">
-						{github.content}
+					<a
+						href={github.content}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="contact-icon-container"
+					>
+						<FontAwesomeIcon
+							icon={icons['github']}
+							className="contact-item-icon"
+						/>
 					</a>
+					<strong>{github.title}</strong>
 				</div>
 			</div>
 		</section>
